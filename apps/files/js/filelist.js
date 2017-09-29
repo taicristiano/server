886de32aa7628bfc0fb280d58bf314e5b6a320ec
Bjoern Schiessle <bjoern@schiessle.org>
2017-09-29 17:15:30 +0200

    hide ... menu for encrypted folders
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -1165,7 +1165,8 @@
 				"data-mtime": mtime,
 				"data-etag": fileData.etag,
 				"data-permissions": fileData.permissions || this.getDirectoryPermissions(),
-				"data-has-preview": fileData.hasPreview !== false
+				"data-has-preview": fileData.hasPreview !== false,
+				"data-e2eencrypted": (type === 'dir' && fileData.isEncrypted)
 			});
 
 			if (dataIcon) {
